c98d18c65a1dc66db3dd8a2632549ca0e31b2516
AncientXYZ <zhaoyang2003@outlook.com>
2024-01-30 22:54:47 -0800

    first

---
+++ test.py
@@ -0,0 +1,19 @@
+def integer_operations(a, b):
+    print("The difference between the two numbers:", a - b)
+    print("The product of the two numbers:", a * b)
+    print("The average of the two numbers:", (a + b) / 2)
+    print("The distance (absolute value of the difference):", abs(a - b))
+    print("The maximum (the larger of the two):", max(a, b))
+    print("The minimum (the smaller of the two):", min(a, b))
+
+# Example usage:
+# a = int(input("Enter the first integer: "))
+# b = int(input("Enter the second integer: "))
+# integer_operations(a, b)
+
+# Since I can't take user input in this environment, I will use two example integers:
+example_a = 15
+example_b = 7
+
+# The results with the example integers are:
+integer_operations(example_a, example_b)
